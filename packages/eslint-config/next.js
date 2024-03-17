8e94272fbb690d0ecc0612681b011850eb96d9b1
Angel S. Moreno <angelxmoreno@gmail.com>
2024-03-17 16:25:28 -0400

    chore: installed and configured ESLint, Prettier, Husky, LintStaged and Conventional Commits

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -1,35 +1,17 @@
-const { resolve } = require("node:path");
-
-const project = resolve(process.cwd(), "tsconfig.json");
-
+/*
+ * base eslint config for NExt.js apps
+ */
+const reactInternal = require("./react-internal");
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: [
-    "eslint:recommended",
-    "prettier",
-    require.resolve("@vercel/style-guide/eslint/next"),
-    "eslint-config-turbo",
-  ],
-  globals: {
-    React: true,
-    JSX: true,
-  },
-  env: {
-    node: true,
-    browser: true,
-  },
-  plugins: ["only-warn"],
-  settings: {
-    "import/resolver": {
-      typescript: {
-        project,
-      },
-    },
-  },
-  ignorePatterns: [
-    // Ignore dotfiles
-    ".*.js",
-    "node_modules/",
-  ],
-  overrides: [{ files: ["*.js?(x)", "*.ts?(x)"] }],
+    ...reactInternal,
+    extends: [
+        ...reactInternal.extends,
+        require.resolve("@vercel/style-guide/eslint/next"),
+        "universe/node",
+    ],
+    env: {
+        ...reactInternal.env,
+        node: true,
+    }
 };
