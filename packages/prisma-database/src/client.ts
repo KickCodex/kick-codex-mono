bf0b3f5123ba33b3499e3bcaa99dcabcd11ce497
Angel S. Moreno <angelxmoreno@gmail.com>
2024-03-18 13:18:40 -0400

    chore: augmented user model for auth

--- packages/prisma-database/src/client.ts
+++ packages/prisma-database/src/client.ts
@@ -1,3 +1,42 @@
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+type AfterLogInUser = {
+    name: string;
+    email: string;
+    image?: string;
+};
+
+type AfterLogInAccount = {
+    provider: string;
+    providerAccountId: string;
+};
+
+export const prisma = new PrismaClient().$extends({
+    name: 'afterLogIn',
+    model: {
+        user: {
+            async afterLogIn(user: AfterLogInUser, account: AfterLogInAccount) {
+                const existingUser = await prisma.user.findFirst({
+                    where: {
+                        providerName: account.provider,
+                        providerUid: account.providerAccountId,
+                    },
+                });
+                if (existingUser) {
+                    return !existingUser.isBanned;
+                }
+                await prisma.user.create({
+                    data: {
+                        name: user.name,
+                        email: user.email,
+                        image: user.image,
+                        providerName: account.provider,
+                        providerUid: account.providerAccountId,
+                    },
+                });
+
+                return true;
+            },
+        },
+    },
+});
