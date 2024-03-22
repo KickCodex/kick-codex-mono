de62af287fc5b41d1f3439b42cb55a0340120f43
Angel S. Moreno <angelxmoreno@gmail.com>
2024-03-22 12:55:02 -0400

    refactor: coderabbitai suggestions

--- packages/typeorm-database/src/utils/calculateDiff.ts
+++ packages/typeorm-database/src/utils/calculateDiff.ts
@@ -1,10 +1,9 @@
 export const calculateDiff = (original: Record<string, any>, current: Record<string, any>): Record<string, any> => {
-    console.log({ original, current });
     const diff: Record<string, any> = {};
-    for (const key in original) {
+    Object.keys(original).forEach(key => {
         if (key !== 'createdAt' && key !== 'updatedAt' && original[key] !== current[key]) {
             diff[key] = { original: original[key], current: current[key] };
         }
-    }
+    });
     return diff;
 };
